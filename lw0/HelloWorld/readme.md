49640368eb8ee72d4a160f1f1c2e7221c6d81b61
lulldev <lulldev@yandex.ru>
2017-09-12 10:26:58 +0300

    add args printing

--- lw0/HelloWorld/readme.md
+++ lw0/HelloWorld/readme.md
@@ -1,5 +1,5 @@
 <h4>Running lw0 on OSX/Unix</h4>
-```java
+```sh
 javac -d build src/HelloWorld.java
 java -classpath ./build net.volgatech.javacore2017.HelloWorld
 ```
--- lw0/HelloWorld/src/HelloWorld.java
+++ lw0/HelloWorld/src/HelloWorld.java
@@ -2,7 +2,17 @@ package net.volgatech.javacore2017;
 
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, Java from");
+    if (args.length == 0) {
+      System.out.println("First argument are required!");
+      return;
+    }
+
+    System.out.println("Hello, Java from " + args[0]);
+    for(int i = 1; i < args.length; i++) {
+      System.out.print(args[i] + " ");
+    }
+    System.out.println();
+
     System.out.println("OS name: " + System.getProperty("os.name"));
     System.out.println("OS version: " + System.getProperty("os.version"));
     System.out.println("Java version: " + System.getProperty("java.version"));

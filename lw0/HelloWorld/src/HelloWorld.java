7fcd2f5defaa3b1007afaa4e16ddd47418a91589
lulldev <lulldev@yandex.ru>
2017-09-12 10:31:40 +0300

    reformat code

--- lw0/HelloWorld/src/HelloWorld.java
+++ lw0/HelloWorld/src/HelloWorld.java
@@ -1,21 +1,22 @@
 package net.volgatech.javacore2017;
 
 public class HelloWorld {
-  public static void main(String[] args) {
-    if (args.length == 0) {
-      System.out.println("First argument are required!");
-      return;
-    }
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("First argument are required!");
+            return;
+        }
 
-    System.out.println("Hello, Java from " + args[0]);
-    for(int i = 1; i < args.length; i++) {
-      System.out.print(args[i] + " ");
-    }
-    System.out.println();
+        System.out.println("Hello, Java from " + args[0]);
+        for(int i = 1; i < args.length; i++) {
+            System.out.print(args[i] + " ");
+        }
+        System.out.println();
 
-    System.out.println("OS name: " + System.getProperty("os.name"));
-    System.out.println("OS version: " + System.getProperty("os.version"));
-    System.out.println("Java version: " + System.getProperty("java.version"));
-    System.out.println("Java path: " + System.getProperty("java.home"));
-  }
+        System.out.println("OS name: " + System.getProperty("os.name"));
+        System.out.println("OS version: " + System.getProperty("os.version"));
+        System.out.println("Java version: " + System.getProperty("java.version"));
+        System.out.println("Java path: " + System.getenv("JAVA_HOME"));
+        System.out.println("User path: " + System.getProperty("user.dir"));
+    }
 }

0a605417be35d70522e89e1370be425c83780c4e
lulldev <lulldev@yandex.ru>
2017-09-12 10:13:50 +0300

    ready program HelloWorld

--- lw0/HelloWorld/src/HelloWorld.java
+++ lw0/HelloWorld/src/HelloWorld.java
@@ -3,6 +3,9 @@ package net.volgatech.javacore2017;
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, Java from");
-    System.out.println("Hello, Java from");
+    System.out.println("OS name: " + System.getProperty("os.name"));
+    System.out.println("OS version: " + System.getProperty("os.version"));
+    System.out.println("Java version: " + System.getProperty("java.version"));
+    System.out.println("Java path: " + System.getProperty("java.home"));
   }
 }

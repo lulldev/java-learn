65488f711567def1c048ba06733686d2d530c27c
lulldev <lulldev@yandex.ru>
2017-09-12 10:07:56 +0300

    new file structure, add notes

---
+++ lw0/HelloWorld/readme.md
@@ -0,0 +1,5 @@
+<h4>Running lw0 on OSX/Unix</h4>
+```java
+javac -d build src/HelloWorld.java
+java -classpath ./build net.volgatech.javacore2017.HelloWorld
+```
---
+++ lw0/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,8 @@
+package net.volgatech.javacore2017;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, Java from");
+    System.out.println("Hello, Java from");
+  }
+}

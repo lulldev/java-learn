b837cad06326660df9b7820796c9707ee4a1d208
lulldev <lulldev@yandex.ru>
2017-10-10 08:28:34 +0300

    create decrypting

--- lw1/СipherOfCaesar/src/CipherOfCaesar.java
+++ lw1/СipherOfCaesar/src/CipherOfCaesar.java
@@ -8,16 +8,72 @@ public class CipherOfCaesar {
         for (int i = 0; i < textString.length(); i++) {
             char c = chars[i];
             if (c >= 'a' && c <= 'z') {
-                c = (char)(c + (shift % 26));
+                c = (char) (c + (shift % 26));
                 if (c > 'z') {
                     c = (char) ('a' + (c - 'z') - 1);
+                }
+                chars[i] = c;
+            }
+            else if (c >= 'A' && c <= 'Z') {
+                c = (char) (c + (shift % 26));
+                if (c > 'Z') {
+                    c = (char) ('A' + (c - 'Z') - 1);
+                }
+                chars[i] = c;
+            }
+            else if (c >= 'а' && c <= 'я') {
+                c = (char) (c + (shift % 32));
+                if (c > 'я') {
+                    c = (char) ('а' + (c - 'я') - 1);
+                }
+                chars[i] = c;
+            }     
+            else if (c >= 'А' && c <= 'Я') {
+                c = (char) (c + (shift % 32));
+                if (c > 'Я') {
+                    c = (char) ('А' + (c - 'Я') - 1);
+                }
+                chars[i] = c;
+            }            
+        }
+        return new String(chars);
+    }
+
+    public static String CaesarDecrypt(String textString, int shift) {
+        char[] chars = textString.toCharArray();
+        for (int i = 0; i < textString.length(); i++) {
+            char c = chars[i];
+            if (c >= 'a' && c <= 'z') {
+                c = (char) (c + (shift % 26));
+                if (c < 'a') {
+                    c = (char) ('z' - ('a' - c) - 1);
+                }
+                chars[i] = c;
+            }
+            else if (c >= 'A' && c <= 'Z') {
+                c = (char) (c + (shift % 26));
+                if (c < 'А') {
+                    c = (char) ('Z' - ('A' - c) - 1);
+                }
+                chars[i] = c;
+            }
+            else if (c >= 'а' && c <= 'я') {
+                c = (char) (c + (shift % 32));
+                if (c < 'а') {
+                    c = (char) ('я' - ('а' - c) - 1);
+                }
+                chars[i] = c;
+            }     
+            else if (c >= 'А' && c <= 'Я') {
+                c = (char) (c + (shift % 32));
+                if (c < 'А') {
+                    c = (char) ('Я' - ('А' - c) - 1);
                 }
                 chars[i] = c;
             }
         }
         return new String(chars);
     }
-
     public static void main(String[] args) {
         if (args.length != 3) {
             System.out.println("Specify the arguments for the program!");

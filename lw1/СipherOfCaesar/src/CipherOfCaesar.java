b938f27e852b5d92306fd77f99e2722d9be088bf
lulldev <lulldev@yandex.ru>
2017-10-10 10:15:15 +0300

    fix code with issues, in progress

--- lw1/СipherOfCaesar/src/CipherOfCaesar.java
+++ lw1/СipherOfCaesar/src/CipherOfCaesar.java
@@ -7,34 +7,36 @@ public class CipherOfCaesar {
     public static int ERR_ALPHABET_LENGTH       = -4;  
     public static int ERR_MODE_VALUE            = -5;  
     public static int EXIT_SUCCESS              =  0;
+    public static int MAX_RU_ALPHABET_LENGHT    = 32;
+    public static int MAX_EN_ALPHABET_LENGHT    = 26;
 
     public static String caesarEncrypt(String textString, int shift) {
         char[] chars = textString.toCharArray();
         for (int i = 0; i < textString.length(); i++) {
             char c = chars[i];
             if (c >= 'a' && c <= 'z') {
-                c = (char) (c + (shift % 26));
+                c = (char) (c + (shift % MAX_EN_ALPHABET_LENGHT));
                 if (c > 'z') {
                     c = (char) ('a' + (c - 'z') - 1);
                 }
                 chars[i] = c;
             }
             else if (c >= 'A' && c <= 'Z') {
-                c = (char) (c + (shift % 26));
+                c = (char) (c + (shift % MAX_EN_ALPHABET_LENGHT));
                 if (c > 'Z') {
                     c = (char) ('A' + (c - 'Z') - 1);
                 }
                 chars[i] = c;
             }
             else if (c >= 'а' && c <= 'я') {
-                c = (char) (c + (shift % 32));
+                c = (char) (c + (shift % MAX_RU_ALPHABET_LENGHT));
                 if (c > 'я') {
                     c = (char) ('а' + (c - 'я') - 1);
                 }
                 chars[i] = c;
             }     
             else if (c >= 'А' && c <= 'Я') {
-                c = (char) (c + (shift % 32));
+                c = (char) (c + (shift % MAX_RU_ALPHABET_LENGHT));
                 if (c > 'Я') {
                     c = (char) ('А' + (c - 'Я') - 1);
                 }
@@ -49,28 +51,28 @@ public class CipherOfCaesar {
         for (int i = 0; i < textString.length(); i++) {
             char c = chars[i];
             if (c >= 'a' && c <= 'z') {
-                c = (char) (c + (shift % 26));
+                c = (char) (c + (shift % MAX_EN_ALPHABET_LENGHT));
                 if (c < 'a') {
                     c = (char) ('z' - ('a' - c) + 1);
                 }
                 chars[i] = c;
             }
             else if (c >= 'A' && c <= 'Z') {
-                c = (char) (c + (shift % 26));
+                c = (char) (c + (shift % MAX_EN_ALPHABET_LENGHT));
                 if (c < 'А') {
                     c = (char) ('Z' - ('A' - c) + 1);
                 }
                 chars[i] = c;
             }
             else if (c >= 'а' && c <= 'я') {
-                c = (char) (c + (shift % 32));
+                c = (char) (c + (shift % MAX_RU_ALPHABET_LENGHT));
                 if (c < 'а') {
                     c = (char) ('я' - ('а' - c) + 1);
                 }
                 chars[i] = c;
             }     
             else if (c >= 'А' && c <= 'Я') {
-                c = (char) (c + (shift % 32));
+                c = (char) (c + (shift % MAX_RU_ALPHABET_LENGHT));
                 if (c < 'А') {
                     c = (char) ('Я' - ('А' - c) + 1);
                 }
@@ -108,10 +110,10 @@ public class CipherOfCaesar {
 
         switch (args[0]) {
         case "-e":
-            System.out.println(caesarEncrypt(args[2], key));
+            System.out.println(caesarEncrypt(alphabet, key));
             break;
         case "-d":
-            System.out.println(caesarDecrypt(args[2], key));
+            System.out.println(caesarDecrypt(alphabet, key));
             break;
         default:
             System.out.println("Specify mode argument!");

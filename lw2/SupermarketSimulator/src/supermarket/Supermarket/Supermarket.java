1c25554fd04487b5de5d38bca9fb04f9abdaa69c
lulldev <lulldev@yandex.ru>
2018-01-04 17:17:49 +0300

    add base interface structure for supermarket

--- lw2/SupermarketSimulator/src/supermarket/Supermarket/Supermarket.java
+++ lw2/SupermarketSimulator/src/supermarket/Supermarket/Supermarket.java
@@ -1,11 +1,44 @@
 package supermarket.Supermarket;
 
 import supermarket.Customer.Customer;
+import supermarket.Supermarket.CashDesk;
 
 public class Supermarket {
 
+    private boolean isOpen;
     private Customer customerList[];
-    private CashDesk cashDesk;
-    private Report report;
+    private CashDesk cashDesks[];
+
+    public void openMarket() {
+        this.isOpen = true;
+    }
+
+    public void closeMarket() {
+        this.isOpen = false;
+    }
+
+    public void finishWork() {
+        // TODO: show report
+    }
+
+    public void configureMarket() {
+        if (!this.isOpen) {
+            // TODO: add products & prices
+        } else {
+            // TODO: log err
+        }
+    }
+
+    public void addCustomer(Customer customer) {
+        if (this.isOpen) {
+            // TODO: add customer
+        } else {
+            // TODO: show hint
+        }
+    }
+
+    public void deleteCustomer(Customer customer) {
+
+    }
 
 }
